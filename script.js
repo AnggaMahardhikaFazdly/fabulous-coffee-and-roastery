f3216eedc466e811a454532253265e2f5ffdf480
Angga Mahardhika Fazdly <angga.mahard@gmail.com>
2020-11-20 19:54:41 +0700

    add logo, color-nav and bg-color body edited

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ showSlides();
 
 function showSlides() {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
+    let slides = document.getElementsByClassName("sliderCarousel");
     let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -26,4 +26,27 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+
+let catalogList = 1;
+showDivs(catalogList);
+
+function currentDiv(n) {
+    showDivs(catalogList = n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("product");
+    let dots = document.getElementsByClassName("buttonCatalog");
+    if (n > x.length) { catalogList = 1 }
+    if (n < 1) { catalogList = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    x[catalogList - 1].style.display = "flex";
 }
